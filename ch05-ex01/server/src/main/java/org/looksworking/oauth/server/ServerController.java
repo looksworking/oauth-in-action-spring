1ac337f25d4d708eb64a45014cdff1bb625d801c
Vyacheslav Petrukhin <petrukhinv@gmail.com>
2017-11-19 10:51:47 +0300

    ch 5 ex 1 clients list

--- ch05-ex01/server/src/main/java/org/looksworking/oauth/server/ServerController.java
+++ ch05-ex01/server/src/main/java/org/looksworking/oauth/server/ServerController.java
@@ -6,12 +6,19 @@ import org.looksworking.oauth.server.org.looksworking.oauth.server.model.ClientS
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -31,11 +38,17 @@ public class ServerController {
     }
 
     @RequestMapping(path = "/clients", method = RequestMethod.GET)
-    public String getClients(){
+    public String getClients(Model model, HttpServletRequest request, HttpServletResponse response){
 
+        List<ClientEntity> clients = clientRepository.findAll();
+        model.addAttribute("clients", clients);
+        return "clients";
+    }
 
-        logger.info(String.valueOf(clientService.getClientList().size()));
+    @RequestMapping(path = "/authorize", method = RequestMethod.GET)
+    public String authorize(@RequestParam String client_id, HttpServletRequest request, HttpServletResponse response){
 
+        logger.info(client_id);
 
         return "ok";
     }
--- ch05-ex01/server/src/main/java/org/looksworking/oauth/server/org/looksworking/oauth/server/model/ClientRepository.java
+++ ch05-ex01/server/src/main/java/org/looksworking/oauth/server/org/looksworking/oauth/server/model/ClientRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
+    ClientEntity findClientEntityByClientId(String clientId);
 }
